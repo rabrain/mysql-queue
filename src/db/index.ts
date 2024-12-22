4de26039c39a493d8d6bf0216291973c0693d12b
Junjun Deng <cnjamesdeng@gmail.com>
2024-12-22 10:38:43 +0800

    Add test job to the CI workflow

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,13 +2,14 @@ import { MySql2Database, type MySqlRawQueryResult, drizzle } from "drizzle-orm/m
 import { migrate } from "drizzle-orm/mysql2/migrator";
 import mysql from "mysql2/promise";
 import path from "node:path";
+import { env } from 'node:process';
 import * as schema from "./schema";
 
 export const affectedRows = (rawResult: MySqlRawQueryResult) => {
     return rawResult[0].affectedRows
 };
 
-const defaultURL = 'mysql://root:root@localhost:3306/queue'
+const defaultURL = env['DATABASE_URL'] ?? 'mysql://root:root@localhost:3306/queue'
 
 export function buildDBClient(url?: string, runMigrations = false) {
     const connection = mysql.createPool(url ?? defaultURL);
